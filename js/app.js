5bbf8058e62868464375d5c86861dac08a16c5c7
Bing Xie <bing708@gmail.com>
2015-09-26 14:20:31 +1000

    Update slider control on scroll event

--- js/app.js
+++ js/app.js
@@ -2,6 +2,12 @@ window.onload = function() {
   animateLogo();
 
   animateRobot();
+
+  updateSliderControl();
+}
+
+window.onscroll = function() {
+  updateSliderControl();
 }
 
 function animateRobot() {
@@ -30,4 +36,26 @@ function animateLogo() {
       ease: Power2.easeInOut
     }
   );
+}
+
+function updateSliderControl() {
+  var links = document.querySelectorAll("#slider-control a");
+
+  for(var i = 0; i < links.length; i++) {
+    var link = links[i];
+    var sectionName = link.attributes[0].value.substring(1);
+
+    var section = document.getElementById(sectionName);
+    var sectionTop = section.offsetTop;
+    var sectionBottom = section.offsetTop + section.clientHeight;
+
+    // Check if window.scrollY is between the section.
+    var scrolly = Math.round(window.scrollY);
+    if(scrolly >= sectionTop && scrolly < sectionBottom) {
+      link.className = "active";
+    } else {
+      link.className = "";
+    }
+
+  }
 }
